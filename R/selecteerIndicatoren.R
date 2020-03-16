bc64dfee7eeec28a5cd49acfd8bda025ce289f02
LOMMELEN <els.lommelen@inbo.be>
2020-03-16 16:54:35 +0100

    replace complex query by simple query

--- R/selecteerIndicatoren.R
+++ R/selecteerIndicatoren.R
@@ -20,26 +20,9 @@ selecteerIndicatoren <-
     #this function call only gives a warning in lintr if this comment precedes
     assert_that(inherits(Connectie, "DBIConnection"))
 
-    #A CTE query
+    #A simple selection query
     query <-
-      "WITH Habitatselectie
-      AS
-      (
-        SELECT Ht1.Id AS HabitattypeId, Ht1.Id AS HabitatsubtypeId
-        FROM Habitattype AS Ht1
-        WHERE Ht1.ParentId IS NULL
-      UNION ALL
-        SELECT Habitatselectie.HabitattypeId, Ht2.Id AS HabitatsubtypeId
-        FROM Habitatselectie INNER JOIN Habitattype AS Ht2
-        ON Habitatselectie.HabitatsubtypeId = Ht2.ParentId
-      )
-      SELECT Ht1.Code AS Habitattype,
-          Ht2.Code AS Habitatsubtype
-      FROM Habitatselectie
-        INNER JOIN Habitattype Ht1
-          ON Habitatselectie.HabitattypeId = Ht1.Id
-        INNER JOIN Habitattype Ht2
-          ON Habitatselectie.HabitatsubtypeId = Ht2.Id"
+      "SELECT VersieLSVI FROM Versie"
 
     #dbGetQuery does not give a warning
     Selectiegegevens <- dbGetQuery(Connectie, query)
